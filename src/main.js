689f7cb67878b8ecbcbf347b80341f00c95bdaf7
wuguang <guangwu@roseonly.com>
2021-05-17 10:19:36 +0800

    去除mainjs引入elementjs

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Axios from 'axios'
-import ElementUI from 'element-ui';
+// import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import Notification from 'element-ui/lib/notification'
 import MessageBox from 'element-ui/lib/message-box'
